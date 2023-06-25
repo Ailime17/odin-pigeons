1add4a8d8f2a8b1cbe857dcf33c2273008e50fad
Emilia <emilia.skrzypczak.es@gmail.com>
2023-06-25 19:55:40 +0200

    create all controller methods

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,22 +1,42 @@
 class PigeonsController < ApplicationController
   def index
+    @pigeons = Pigeon.all
   end
 
   def show
+    @pigeon = Pigeon.find(params[:id])
   end
 
   def new
+    @pigeon = Pigeon.new
   end
 
   def create
+    @pigeon = Pigeon.new(pigeon_params)
+    if @pigeon.save
+      redirect_to @pigeon
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @pigeon = Pigeon.find(params[:id])
   end
 
   def update
+    @pigeon = Pigeon.find(params[:id])
+    if @pigeon.update(pigeon_params)
+      redirect_to @pigeon
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @pigeon = Pigeon.find(params[:id])
+    @pigeon.destroy
+
+    redirect_to root_path, status: :see_other
   end
 end
